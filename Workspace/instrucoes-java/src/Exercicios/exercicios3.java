7581b0ea039c72cdc97532d613413d5b281277e9
jlog180924 <jlog180924@info.local>
2018-09-28 22:00:26 -0300

    exercicios3

---
+++ Workspace/instrucoes-java/src/Exercicios/exercicios3.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+public class exercicios3 {
+	public static void main(String[] args) {
+		
+	int n = 94830;
+	if (n % 2 == 0) {
+		System.out.println("é um numero par");
+	} else {
+		System.out.println("é um numero impar");
+	}
+}
+
+}
